a0bff431a7e3d4d106a15b5f48f5f18c46e80676
Trung <trungnhph13487@fpt.edu.vn>
2022-03-15 09:09:32 +0700

    a

--- routes/post.js
+++ routes/post.js
@@ -8,6 +8,6 @@ router.get('/posts', checkAuth, list);
 router.get('/post/:id', checkAuth, read);
 router.post('/posts', checkAuth, create);
 router.delete('/post/:id', checkAuth, remove);
-router.patch("/post/:id", checkAuth, update)
+router.put("/post/:id", checkAuth, update)
 
 export default router;
