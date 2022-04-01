b964b8062b99d60e59453c201eebf698cf6b2ce6
Trung <trungnhph13487@fpt.edu.vn>
2022-04-02 06:08:56 +0700

    a

--- middlewares/checkAuth.js
+++ middlewares/checkAuth.js
@@ -1,3 +1,5 @@
+import expressJWT from 'express-jwt';
+
 export const checkAuth = (req, res, next) => {
     const status = true;
     if (status) {
@@ -5,4 +7,28 @@ export const checkAuth = (req, res, next) => {
     } else {
         console.log("Anh không có quyền truy cập");
     }
+
+}
+
+export const requireSignin = expressJWT({
+    algorithms: ["HS256"],
+    secret: "123456",
+    requestProperty: "auth" // req.auth    
+});
+export const isAuth = (req, res, next) => {
+    const status = req.profile._id == req.auth._id;
+    if (!status) {
+        res.status(400).json({
+            message: "Ban khong co quyen truy cap"
+        })
+    }
+    next();
+}
+export const isAdmin = (req, res, next) => {
+    if (req.profile.role === 0) {
+        res.status(401).json({
+            message: "Bạn không phải là admin"
+        })
+    }
+    next();
 }
--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { create, read, remove, update, list } from '../controllers/product';
 import { checkAuth } from '../middlewares/checkAuth'
+import { userById } from '../controllers/user';
 const router = Router();
 
 // resful API
@@ -9,5 +10,5 @@ router.get('/products/:id', checkAuth, read);
 router.post('/products', checkAuth, create);
 router.delete('/products/:id', checkAuth, remove);
 router.put("/products/:id", checkAuth, update)
-
+router.param("userId", userById);
 export default router;
