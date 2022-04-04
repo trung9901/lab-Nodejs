5082f817fe12a0446411cd65a5b0e0b2ce5b9c82
Trung <trungnhph13487@fpt.edu.vn>
2022-04-05 02:21:38 +0700

    a

--- routes/category.js
+++ routes/category.js
@@ -4,10 +4,10 @@ import { userById } from "../controllers/user";
 import { checkAuth, isAuth, isAdmin, requireSignin } from "../middlewares/checkAuth";
 const router = Router();
 
-router.get('/categories', checkAuth, list)
-router.get('/categories/:id', checkAuth, read)
-router.post('/categories/:userId', requireSignin, isAuth, isAdmin, post)
-router.put('/categories/:userId/:id', checkAuth, update)
-router.delete('/categories/:userId/:id', checkAuth, remove)
+router.get('/categories', list)
+router.get('/categories/:id', read)
+router.post('/categories/:userId', post)
+router.put('/categories/:userId/:id', update)
+router.delete('/categories/:userId/:id', remove)
 router.param('userId', userById)
 export default router;
