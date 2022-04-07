d2254bcf6c5e964577445ace7cf650b345249c2d
trung9901 <trungnhph13487@fpt.edu.vn>
2022-04-07 10:29:25 +0700

    a

--- models/product.js
+++ models/product.js
@@ -1,19 +1,38 @@
 import mongoose, { Schema, ObjectId } from 'mongoose';
+
 const productSchema = new Schema({
     name: {
         type: String,
         minLength: 5,
-
+        required: true,
         unique: true
     },
+    // slug: {
+    //     type: String,
+    //     lowercase: true,
+    //     unique: true,
+    //     index: true
+    // },
     price: {
         type: Number,
-
+        required: true
     },
-    // category: {
-    //     type: ObjectId,
-    //     ref: "Category"
-    // }
+    description: {
+        type: String,
+        required: true
+    },
+    // image: {
+    //     type: String,
+    //     required: true
+    // },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: ObjectId,
+        ref: "category"
+    }
 
 }, { timestamps: true })
 productSchema.index({ "$**": 'text' });
